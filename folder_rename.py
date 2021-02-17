114dec45f8048d75244b7c426569f005f62e2fba
rmsmsgood <rmsmsgood@naver.com>
2021-02-18 04:55:52 +0900

    알고리즘 카테고리 개정 완료

--- folder_rename.py
+++ folder_rename.py
@@ -8,10 +8,15 @@ caution = input("정말로 사용하려면 y를 입력해주세요")
 if caution != "y":
     raise
     
-for author in ["j_", "r_"]:
+# for author in ["j_", "r_"]:
+for author in ["r_"]:
     depth1 = os.path.join(author)
     print(depth1)
     for category in os.listdir(depth1):
+    # for category in ["0_recent"]:
+        if category == "main":
+            print("mian")
+            continue
         depth2 = os.path.join(depth1, category)
         print(depth2)
         for post in os.listdir(depth2):
@@ -21,12 +26,19 @@ for author in ["j_", "r_"]:
             index = index_md.readlines()
             index_md.close()
             # 폴더이름 맞춰줌
-            # title = index[1]
-            # title = title[title.find('"')+1:]
-            # title = title[0:title.find('"')]
+            title = index[1]
+            title = title[title.find('"')+1:]
+            title = title[0:title.find('"')]
+            title = title.replace("\\","＼").replace("/","／").replace(" ","").replace("-","").replace("!","！").replace("?","？").replace(":","：").replace("n","ｎ")
+            title = title.replace("에서","").replace("의","").replace("증명","").replace("유도","").replace("알고리즘","").replace("하는법","").replace("시뮬레이션","")
+            title = title.replace("모델","m").replace("정리","t").replace("함수","f").replace("평균과분산","2")
+            title = title[-6:]
             # slug = index[2]
             # slug = slug[slug.find('"')+1:]
             # slug = slug[0:slug.find('"')]
+            
+            # os.rename(depth3, os.path.join(depth2, post[0:4]).replace("?",""))
             # os.rename(depth3, os.path.join(depth2, post[0:4] + "_" + title + "_" + slug).replace("?",""))
-            input()
-        input()
+            os.rename(depth3, os.path.join(depth2, index[9][5:-1].zfill(4) + "_" + title))
+            # input()
+        # input()
